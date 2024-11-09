d5176716af0f142c6550b8bd4c6fff136b85af64
Christian Niklas <christian.niklas@outletcity.com>
2024-11-09 18:03:42 +0100

    update type `RecipeFormData`

--- src/types/Recipe.type.ts
+++ src/types/Recipe.type.ts
@@ -13,17 +13,8 @@ export type Recipe = {
 	serves: number
 }
 
-export type RecipeFormData = {
-	category_id: number
-	complexity: number
-	cook_book_id: number
+export type RecipeFormData = Omit<Recipe, 'duration' | 'id' | 'serves'> & {
 	duration: number | string
 	id?: number
-	ingredients: string
-	leftovers: boolean
-	name: string
-	notes: string
-	recommended: boolean
-	remarkable_ingredients: string
 	serves: number | string
 }
